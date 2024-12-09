b522c8c648a00c2142c523743dc5dd0847eb5ffd
LuizFelipeFF <Felipoyd1912@gmail.com>
2024-12-09 11:49:35 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Java?',
+    'O Java é uma linguagem de programação'
+)
+
+criaCartao(
+    'Jogos',
+    'Qual o jogo mais famoso feito no Java',
+    'Minecraft'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um jogo Indi?',
+    'Jogo Inde é um jogo feito por poucas ou até mesmo uma pessoa'
+)
+
+criaCartao(
+    'Jogos/Inglês',
+    'Por que jogos de tiro são chamados de FPS?',
+    'Por que são jogos de tiro em primeira pessoa, sendo chamados de First-Person Shooters'
+)
